9e89c49817e65f33e6418cb8777e67e94377ac7b
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2024-12-10 05:42:13 -0800

    req.body instead of param and use express.json() middleware to acess it body as json

---
+++ controllers/UsersController.js
@@ -0,0 +1,31 @@
+import { createHash } from 'crypto';
+import DBClient from '../utils/db';
+
+class UsersController {
+  static async postNew(req, res) {
+    const { email, password } = req.body;
+    if (!email) {
+      return res.status(400).send({ error: 'Missing email' });
+    }
+    if (!password) {
+      return res.status(400).send({ error: 'Missing password' });
+    }
+    if (email) {
+      const response = await DBClient.client.db().collection('users').findOne({ email });
+      if (response) {
+        return res.status(400).send({ error: 'Already exist' });
+      }
+      const shap = createHash('sha1');
+      const hasPass = shap.update(password).digest('hex');
+      const doc = {
+        email,
+        password: hasPass,
+      };
+      const result = await DBClient.client.db().collection('users').insertOne(doc);
+      return res.status(201).send({ id: result.insertedId, email });
+    }
+    return 0;
+  }
+}
+
+export default UsersController;
